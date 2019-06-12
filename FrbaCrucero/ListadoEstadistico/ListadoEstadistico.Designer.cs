54a9343ea7c0fae47fdaee7aa2b4feba6ae97229
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-06-12 01:02:15 -0300

    Se avanza con la abm de listado estadistico

---
+++ FrbaCrucero/ListadoEstadistico/ListadoEstadistico.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace FrbaCrucero
+{
+    partial class ListadoEstadistico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.RECORRIDO_CODIGO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pasajesComprados = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(9, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(622, 458);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listado Estadístico";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione el año a consultar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione el semestre a consultar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Consultar TOP 5:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2017",
+            "2018",
+            "2019"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Primer Semestre",
+            "Segundo Semestre"});
+            this.comboBox2.Location = new System.Drawing.Point(369, 45);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(161, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Recorridos con más pasajes comprados",
+            "Recorridos con más cabinas libres",
+            "Cruceros con mayor cantidad de días fuera de servicio"});
+            this.comboBox3.Location = new System.Drawing.Point(74, 94);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(456, 24);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(46, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(521, 53);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "CONSULTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RECORRIDO_CODIGO,
+            this.pasajesComprados});
+            this.dataGridView1.Location = new System.Drawing.Point(20, 218);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 208);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // RECORRIDO_CODIGO
+            // 
+            this.RECORRIDO_CODIGO.HeaderText = "RECORRIDO_CODIGO";
+            this.RECORRIDO_CODIGO.Name = "RECORRIDO_CODIGO";
+            // 
+            // pasajesComprados
+            // 
+            this.pasajesComprados.HeaderText = "pasajesComprados";
+            this.pasajesComprados.Name = "pasajesComprados";
+            // 
+            // ListadoEstadistico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 474);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ListadoEstadistico";
+            this.Text = "UTN FRBA";
+            this.Load += new System.EventHandler(this.ListadoEstadistico_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RECORRIDO_CODIGO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pasajesComprados;
+    }
+}
---
+++ FrbaCrucero/ListadoEstadistico/ListadoEstadistico.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FrbaCrucero
+{
+    public partial class ListadoEstadistico : Form
+    {
+        string fechaInicial;
+        string fechaFinal;
+
+        public ListadoEstadistico()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void ListadoEstadistico_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResolverListado(SqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+
+                dataGridView1.Rows.Add(reader.GetDecimal(0).ToString(), reader.GetInt32(1).ToString());
+
+            }
+
+            reader.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            if (String.IsNullOrWhiteSpace(comboBox1.Text) || String.IsNullOrWhiteSpace(comboBox2.Text) || String.IsNullOrWhiteSpace(comboBox3.Text))
+            {
+                MessageBox.Show("Por favor complete todos los campos", "Error");
+            }
+            else
+            {
+                if(string.Compare("Primer Semestre", comboBox2.Text)==0){
+                    fechaInicial = comboBox1.Text + "-01-01 00:00:00.000";
+                    fechaFinal = comboBox1.Text + "-07-01 00:00:00.000";
+                } else {
+                    fechaInicial = comboBox1.Text + "-07-01 00:00:00.000";
+                    fechaFinal = comboBox1.Text + "-12-31 00:00:00.000";
+                }
+
+                if(string.Compare("Recorridos con más pasajes comprados", comboBox3.Text)==0){
+                   
+                    string query = "SELECT TOP 5 via.RECORRIDO_CODIGO, COUNT(pas.PASAJE_CODIGO) pasajesComprados FROM ZAFFA_TEAM.Pasaje pas JOIN ZAFFA_TEAM.Viaje via ON pas.VIAJE_ID = via.VIAJE_ID WHERE via.FECHA_SALIDA BETWEEN '" + fechaInicial +"' AND +'"+ fechaFinal +"' AND via.FECHA_LLEGADA BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"' group by via.RECORRIDO_CODIGO order by pasajesComprados desc";
+                   
+                    try
+                    {
+                        ResolverListado(ClaseConexion.ResolverConsulta(query));
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se encontraron viajes en determinada fecha", "Error");
+                    }
+                }
+                if (string.Compare("Recorridos con más cabinas libres", comboBox3.Text) == 0)
+                {
+                   
+                    string query = "SELECT TOP 5 via.RECORRIDO_CODIGO, COUNT(pas.PASAJE_CODIGO) pasajesComprados FROM ZAFFA_TEAM.Pasaje pas JOIN ZAFFA_TEAM.Viaje via ON pas.VIAJE_ID = via.VIAJE_ID WHERE via.FECHA_SALIDA BETWEEN '" + fechaInicial +"' AND +'"+ fechaFinal +"' AND via.FECHA_LLEGADA BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"' group by via.RECORRIDO_CODIGO order by pasajesComprados desc";
+                   
+                    try
+                    {
+                        ResolverListado(ClaseConexion.ResolverConsulta(query));
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se encontraron viajes en determinada fecha", "Error");
+                    }
+                }
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
